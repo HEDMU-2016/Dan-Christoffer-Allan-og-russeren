dfc5462dae89850f36e08563a409f8746ae940f5
agger <agger@DESKTOP-8K59JC7>
2016-11-22 13:18:38 +0100

    Java Fx

---
+++ Bank/src/javafx/Bank_IO.java
@@ -0,0 +1,10 @@
+package javafx;
+
+public class Bank_IO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Bank/src/javafx/Bank_IO2.java
@@ -0,0 +1,16 @@
+package javafx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Bank_IO2 extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
