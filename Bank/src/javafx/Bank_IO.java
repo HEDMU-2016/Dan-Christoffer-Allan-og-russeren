07686f147ebff936a312d82bdeca8cc3a152206f
agger <agger@DESKTOP-8K59JC7>
2016-11-22 13:28:09 +0100

    asdasd

--- Bank/src/javafx/Bank_IO.java
+++ Bank/src/javafx/Bank_IO.java
@@ -4,15 +4,19 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Bank_IO extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-		primaryStage.setTitle("JavaFX Welcome");
+		primaryStage.setTitle("De uduelige's bank");
 
 		primaryStage.show();
 
@@ -28,7 +32,18 @@ public class Bank_IO extends Application {
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		primaryStage.setScene(scene);
-		
+		Text scenetitle = new Text("De uduelige's bank");
+		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+		grid.add(scenetitle, 0, 0, 10, 1);
+
+		TextField userTextField = new TextField();
+		grid.add(userTextField, 1, 1, 10, 1);
+
+		TextField TextField = new TextField();
+		// setEditable(false)
+		grid.add(TextField, 1, 2, 10, 1);
+		TextField.setEditable(false);
+
 	}
 
 	public static void main(String[] args) {
