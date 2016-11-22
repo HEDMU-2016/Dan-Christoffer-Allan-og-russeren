3bdd3e60feaf2729da52b58f4523ca50b1b72d90
agger <agger@DESKTOP-8K59JC7>
2016-11-22 13:24:51 +0100

    rettelse af java fx - Nu med scene!

--- Bank/src/javafx/Bank_IO.java
+++ Bank/src/javafx/Bank_IO.java
@@ -1,12 +1,33 @@
 package javafx;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Bank_IO extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
+		primaryStage.setTitle("JavaFX Welcome");
+
+		primaryStage.show();
+
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.TOP_CENTER);
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(25, 25, 25, 25));
+
+		Scene scene = new Scene(grid, 400, 375);
+		StackPane root = new StackPane();
+		root.setId("pane");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		primaryStage.setScene(scene);
 		
 	}
 
