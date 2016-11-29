a6abb3b002dbd0029576fbca2b39b08e37dabbf2
tanddant <tanddant@gmail.com>
2016-11-29 12:11:21 +0100

    asd

--- Bank/src/javafx/Bank_IO.java
+++ Bank/src/javafx/Bank_IO.java
@@ -1,12 +1,18 @@
-package javafx;
-
+package application;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import static javafx.geometry.HPos.RIGHT;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -14,39 +20,62 @@ import javafx.stage.Stage;
 
 public class Bank_IO extends Application {
 
-	@Override
-	public void start(Stage primaryStage) {
-		primaryStage.setTitle("De uduelige's bank er gay");
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX Welcome");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
 
-		primaryStage.show();
+        Text scenetitle = new Text("Welcome");
+        scenetitle.setId("welcome-text");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
 
-		GridPane grid = new GridPane();
-		grid.setAlignment(Pos.TOP_CENTER);
-		grid.setHgap(10);
-		grid.setVgap(10);
-		grid.setPadding(new Insets(25, 25, 25, 25));
+        Label userName = new Label("User Name:");
+        grid.add(userName, 0, 1);
 
-		Scene scene = new Scene(grid, 400, 375);
-		StackPane root = new StackPane();
-		root.setId("pane");
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		primaryStage.setScene(scene);
-		Text scenetitle = new Text("De uduelige's bank");
-		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-		grid.add(scenetitle, 0, 0, 10, 1);
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
 
-		TextField userTextField = new TextField();
-		grid.add(userTextField, 1, 1, 10, 1);
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
 
-		TextField TextField = new TextField();
-		// setEditable(false)
-		grid.add(TextField, 1, 2, 10, 1);
-		TextField.setEditable(false);
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
 
-	}
+        Button btn = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+        Text actiontarget = new Text();
+        grid.add(actiontarget, 0, 6);
+        grid.setColumnSpan(actiontarget, 2);
+        grid.setHalignment(actiontarget, RIGHT);
+        actiontarget.setId("actiontarget");
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent e) {
+                actiontarget.setFill(Color.FIREBRICK);
+                actiontarget.setText("Tillykke, du har trykket \"Sign in\"\nKnappen gør ikke noget du\nkan bare lukke programmet nu");
+            }
+        });
+
+        Scene scene = new Scene(grid, 300, 275);
+        primaryStage.setScene(scene);
+        scene.getStylesheets().add
+        (Main.class.getResource("Main.css").toExternalForm());
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
