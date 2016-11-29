a03eda15edcbbc575853ab44143c0809f14c674e
agger <agger@DESKTOP-8K59JC7>
2016-11-29 12:41:24 +0100

    hjælp

---
+++ Bank/src/data/login.java
@@ -0,0 +1,23 @@
+package data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class login {
+	static void main(String[] args) throws SQLException {
+		Connection connection =
+		DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/mydb", "SA", "");
+		PreparedStatement statement = connection.prepareStatement("SELECT users");
+		ResultSet resultset = statement.executeQuery();
+		while (resultset.next()) {
+		String user = resultset.getString("user");
+		System.out.println(user);
+		}
+		resultset.close();
+		statement.close();
+		connection.close();
+		}
+}
